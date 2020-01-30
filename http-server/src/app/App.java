b01ce1a0d4c28b1c7bb8a75120c66b6ce7944e86
Oluwasayo <oluwasayoakinkunmi@gmail.com>
2020-01-30 10:52:21 +0100

    refactored code

--- http-server/src/app/App.java
+++ http-server/src/app/App.java
@@ -1,92 +1,78 @@
 package app;
 
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.io.IOException;
 
 /**
  * Server
+ * 
+ *authored by @ unclejide1
  */
+
 public class App {
-  static boolean quit = false;
-  static final int port = 8081;
-  static Path path = Paths.get("http-server\\src\\app\\sample.html");
-  static Path path2 = Paths.get("http-server\\src\\app\\myjson.json");
+  static final int port = 9000;
+  static Path htmlPath = Paths.get("http-server\\src\\app\\sample.html");
+  static Path jsonPath = Paths.get("http-server\\src\\app\\myjson.json");
+
+  public static StringBuilder BuildString(List<String> file) {
+    StringBuilder finalResult = new StringBuilder();
+    for (String line :file) {
+      finalResult.append(line);
+  }
+  return finalResult;
+  }
+  
 
   public static void main(String[] args) {
     System.out.printf("Server is waiting for connection on port %d\n", port);
     try (ServerSocket serverSocket = new ServerSocket(port)) {
-        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-        List<String> lines2 = Files.readAllLines(path2, StandardCharsets.UTF_8);
-      Socket client = serverSocket.accept();
-      System.out.println("New user connected");
-      InputStream is = client.getInputStream();
-      BufferedReader br = new BufferedReader(new InputStreamReader(is));
-      OutputStream ous = client.getOutputStream();
-      String message = "";
-      int headers = 0;
-      while (!quit) {
-        // when connecting with a browser, read all first before returning the response
-        String newMessage = null;
-        String method = null;
-        if(headers == 0){
-             newMessage = br.readLine();
-        message += newMessage;
-        String[] arrayOfMethods = message.split(" ");
-        method = arrayOfMethods[1];
-        // System.out.println(Arrays.toString(arrayOfMethods));
-        }
-        headers++;
-        System.out.printf("Client: %s\n", message);
-        if (message.equals("quit")) {
-          message = "Thanks for chatting with me...";
-          quit = true;
-        } else {
-            String httpResponse = null;
-          if (!client.isClosed()) {
-            StringBuilder finalResult = new StringBuilder();
-            
-            if(method.equals("/")){
-                for (String line : lines) {
-                    finalResult.append(line);
+        List<String>htmlFile = Files.readAllLines(htmlPath, StandardCharsets.UTF_8);
+        List<String>jsonFile = Files.readAllLines(jsonPath, StandardCharsets.UTF_8);
+      String newMessage = "";
+
+      while (true) {
+        Socket client = serverSocket.accept();
+        System.out.println("New user connected");
+        InputStream is = client.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        OutputStream ous = client.getOutputStream();
+        String  route = null;
+        newMessage = br.readLine();
+                if (newMessage.contains("GET ")){
+                String[] arrayOfMethods = newMessage.split(" ");
+                route = arrayOfMethods[1];
                 }
-                httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + finalResult; 
-            }
-            if(method.equalsIgnoreCase("/json")){
-                for (String line : lines2) {
-                    finalResult.append(line);
-                }
-                httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + finalResult; 
-            }
+       
+        StringBuilder routeResponse = new StringBuilder();
+        String contentType = null;
+
             
+                if(route.equals("/")){
+                  routeResponse = BuildString(htmlFile);
+                  contentType = "Content-Type: text/html\r\n" ;
+                }
+
+                if(route.equalsIgnoreCase("/json")){
+                  routeResponse = BuildString(jsonFile);
+                  contentType = "Content-Type: application/json\r\n" ;
+                }
+
+          String httpResponse = "HTTP/1.1 200 OK\r\n" + contentType  + "\r\n" +routeResponse;
               ous.write((httpResponse).getBytes());
               ous.flush();
               ous.close();
-              quit = true;
-          } else {
-            System.err.println("Connection to client is closed!");
-            quit = true;
           }
       }
-    }
-}
      catch (IOException e) {
 
     }
